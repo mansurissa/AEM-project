66adb7fcc690ec691a25da7db54ce29859966845
mansurissa6 <mansurissa6@gmail.com>
2021-12-17 12:18:05 +0200

    my fist aem project

---
+++ core/src/main/java/it/codeland/academy/core/models/Day13.java
@@ -0,0 +1,42 @@
+package it.codeland.academy.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.*;import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Locale;
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class Day13 {
+
+  @Inject
+  @Named("jcr:title")
+  @Required
+  private String title;
+  @Inject
+  @Named("customDescription")
+  private String description;
+  @PostConstruct
+  protected void init() {
+    if (description != null && !description.isEmpty()) {
+      this.description = this.description.toUpperCase(Locale.ROOT);
+    } else {
+      this.description = "Not Available";
+    }
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
